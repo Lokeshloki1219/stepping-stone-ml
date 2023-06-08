81cd96a699a90700bae9e642c3bbaee2fb8d301d
Lokeshloki1219 <lokeshloki1219143@gmail.com>
2023-06-08 20:17:48 +0530

    exception

--- src/exception.py
+++ src/exception.py
@@ -15,10 +15,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-if __name__=="__main__":
-    try:
-        a=l/ø
-    except Exception as e:
-        logging. info("Divide by Zero")
-        raise CustomException(e, sys)
